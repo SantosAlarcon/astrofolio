b9c8ec4a7cbc794fc945cf36c3ee1383a737c2e6
Santos Alarcón <santosalarcon86@gmail.com>
2025-02-13 19:33:03 +0100

    feat: testimonial section

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,4 +13,25 @@ const work = defineCollection({
 	}),
 });
 
-export const collections = { work };
+const post = defineCollection({
+    loader: glob({ pattern: "**/*.mdx", base: "./src/content/blog" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        cover: z.string(),
+        publishedDate: z.date(),
+        updatedDate: z.date(),
+        tags: z.string().array()
+    }),
+});
+
+const testimonial = defineCollection({
+    loader: glob({ pattern: "**/*.mdx", base: "./src/content/testimonials" }),
+    schema: z.object({
+        name: z.string(),
+        title: z.string(),
+        image: z.string()
+    }),
+});
+
+export const collections = { work, post, testimonial };
